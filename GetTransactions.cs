20cbf62d3ae2b4640b0a586747eb7888cf93b697
RiddickNataliia <nataliia.riddick@student.howest.be>
2025-10-02 17:36:41 +0200

    coffee service

--- GetTransactions.cs
+++ GetTransactions.cs
@@ -1,9 +1,10 @@
+using System.Transactions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace Company.Function;
+namespace Howest.Functions;
 
 public class GetTransactions
 {
@@ -17,7 +18,9 @@ public class GetTransactions
     [Function("GetTransactions")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "transactions")] HttpRequest req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        TransactionRepository repository = new TransactionRepository();
+        var transactions = repository.GetAllTransactionsAsync();
+        // _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
     }
 }
---
+++ Models/CoffeeTransaction.cs
@@ -0,0 +1,11 @@
+namespace Howest.Functions;
+
+public class CoffeeTransaction
+{
+    public Guid ID { get; set; }
+    public DateTime DateTime { get; set; }
+    public string? CashType { get; set; }
+    public string? Card { get; set; }
+    public decimal Money { get; set; }
+    public string? CoffeeName { get; set; }
+}
